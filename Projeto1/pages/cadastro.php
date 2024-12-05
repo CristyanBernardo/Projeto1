d468964333637da3c8b66a4a8fc57f40b0d7c3e2
CN <cristyan.xd@hotmail.com>
2024-12-05 13:47:37 -0300

    Pagina de Cadastro

--- Projeto1/pages/cadastro.php
+++ Projeto1/pages/cadastro.php
@@ -4,40 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
-    <link rel="stylesheet" href="../Assets/css/header.css"> <!-- Carregue primeiro o CSS do cabeçalho -->
-    <link rel="stylesheet" href="../Assets/css/cadastro.css"> <!-- Carregue primeiro o CSS do cabeçalho -->
-    <script src="../Assets/js/cadastro.js" defer></script>
-    
+    <link rel="stylesheet" href="../Assets/css/header.css">
+    <link rel="stylesheet" href="../Assets/css/cadastro.css">
+    <script src="../Assets/js/cadastro.js" defer></script> <!-- Incluindo JavaScript -->
 </head>
 <body>
     <?php include_once '../Includes/header.php'?> <!-- Incluindo cabeçalho -->
-
-    <main>
+    
+    <main class="cadastro-main">
         <form action="cadastro.php" method="post">
             <div class="form-group">
                 <label for="nome">Nome</label>
-                <input type="text" id="nome" name="nome" required>
+                <input type="text" id="nome" name="nome"  placeholder="Digite seu nome completo"required>
+        </div>
             </div>
             <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" id="email" name="email" required>
+                <input type="email" id="email" name="email" placeholder="Digite seu Email" required>
             </div>
             <div class="form-group">
                 <label for="senha">Senha</label>
-                <input type="password" id="senha" name="senha" required>
+                <input type="password" id="senha" name="senha" placeholder="Digite sua senha" required>
             </div>
             <div class="form-group">
                 <label for="confirmar_senha">Confirmar Senha</label>
-                <input type="password" id="confirmar_senha" name="confirmar_senha" required>
+                <input type="password" id="confirmar_senha" name="confirmar_senha" placeholder="Confirme sua senha" required>
             </div>
             <div class="form-group">
                 <label for="telefone">Telefone</label>
-                <input type="tel" id="telefone" name="telefone" required>
+                <input type="tel" id="telefone" name="telefone" placeholder="(xx) xxxxx-xxxx" required>
             </div>
             <button type="submit">Cadastrar</button> <!-- Botão de envio -->
         </form>
     </main>
 
-    <script src="../Assets/js/cadastro.js" defer></script> <!-- Incluindo JavaScript -->
 </body>
 </html>
